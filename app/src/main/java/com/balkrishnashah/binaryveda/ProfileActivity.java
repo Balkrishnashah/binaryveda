13666f2cdc6e7cffc6c456bb99268bf89e2e7c95
Balkrishnashah <shahkrishna493@gmail.com>
2021-07-02 22:15:04 +0530

    two screen added

---
+++ app/src/main/java/com/balkrishnashah/binaryveda/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.balkrishnashah.binaryveda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
